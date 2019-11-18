e3168de701919656d72810100efac1a61c6465f8
bennyrhysgz01 <bennyrhys_gz01@163.com>
2019-11-18 10:33:07 +0800

    first send

---
+++ src/com/bennyrhys/Hello.java
@@ -0,0 +1,10 @@
+package com.bennyrhys;
+
+/**
+ * Created by lenovo on 2019/11/18.
+ */
+public class Hello {
+    public static void mian(String[] args){
+        System.out.println("hello");
+    }
+}
